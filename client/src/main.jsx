5cb3e090bf7400692672c9c1ff1f070c397bf9ce
Dipesh-Theeng-Tamang <dt412242@gmail.com>
2025-09-07 21:04:07 +0545

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,9 +6,7 @@ import "./index.css"; // Tailwind CSS and custom styles
 ReactDOM.createRoot(document.getElementById("root")).render(
 
   <React.StrictMode>
-
     <App />
-    
   </React.StrictMode>
 
 );
